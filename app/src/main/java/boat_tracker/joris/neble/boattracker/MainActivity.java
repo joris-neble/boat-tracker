1151784971b35be4e037bb77cfd401326f31c4b1
n17007769 <joris.neble@etu.univ-amu.fr>
2019-04-09 15:18:30 +0200

    Add Step 2 and finish it

--- app/src/main/java/boat_tracker/joris/neble/boattracker/MainActivity.java
+++ app/src/main/java/boat_tracker/joris/neble/boattracker/MainActivity.java
@@ -19,19 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayList<String> listContainers = new ArrayList<>();
-        Containership Bato1 = new Containership(0, "Titanic", "Michel");
-        Containership Bato2 = new Containership(1, "Bato", "roiBateau");
-        listContainers.add(Bato1.toString());
-        listContainers.add(Bato2.toString());
-        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, listContainers);
+        final ArrayList<Containership> listContainers = new ArrayList<>();
+        Containership bato1 = new Containership(0, "Titanic", "Michel");
+        Containership bato2 = new Containership(1, "Bato", "roiBateau");
+        listContainers.add(bato1);
+        listContainers.add(bato2);
+        final AdapterContainership adapter = new AdapterContainership(listContainers,this);
 
         final ListView listView = (ListView) findViewById(R.id.listOfBoat);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), listContainers.get(position) , Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Le nom du bato est : " + listContainers
+                        .get(position).getName() + " et le chef est : " + listContainers.get(position).getCaptainName(), Toast.LENGTH_SHORT).show();
             }
         });
     }
